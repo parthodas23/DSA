c436c38181f2a105cde058bade489846758c47db
Partha Das <parthodasm@gmail.com>
2025-07-04 11:14:00 +0600

    Prob 10

--- Array & String/10_Remove_Dup_Sort_Arr.js
+++ Array & String/10_Remove_Dup_Sort_Arr.js
@@ -1,11 +1,14 @@
 var removeDuplicates = function (nums) {
-  let nonDup = [];
-  for (let i = 0; i < nums.length - 1; i++) {
-    if (nums[i] === nums[i + 1]) {
-      nums[i] = nums[i + 1];
+  let i = 0; // this it slow pointer like just adding unique nums index wise
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] != nums[i]) {
+      // 1st change index then add
+      i++; // this is add and also count how many unique num add like index wise,
+      nums[i] = nums[j];
     }
   }
-  console.log(nums);
+  console.log(nums.slice(0, i + 1));
+  return i + 1; // in here i is for index so it's counts as a index that's totlat unique nums is i+1
 };
 
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
