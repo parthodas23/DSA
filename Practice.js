c3ddb702749814a5bc9abad15ff80e395e35895b
Partha Das <parthodasm@gmail.com>
2025-09-20 14:30:15 +0600

    Practice

--- Practice.js
+++ Practice.js
@@ -1,7 +1,7 @@
 class Node {
   constructor(value) {
     this.value = value;
-    this.next = null; // initially, no next node
+    this.next = null;
   }
 }
 
@@ -11,32 +11,29 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
-
   push(value) {
     const newNode = new Node(value);
     if (!this.head) {
-      // if head doesn't exist then head and tail would be same
       this.head = newNode;
       this.tail = newNode;
     } else {
-      this.tail.next = newNode; // this tail is for to pointing the prev val to last val
-      this.tail = newNode; // then set tail= 20 the last one
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-
     this.length++;
-    return this; // it's return the hole linked list
+    return this;
   }
 
   pop() {
     if (!this.head) return undefined;
     let current = this.head;
-    let prev = current;
+    let prev;
     while (current.next) {
-      prev = current; // prev is always behaind to current
+      prev = current;
       current = current.next;
     }
     this.tail = prev;
-    this.tail.next = null; // set there are no value would be left
+    this.tail.next = null;
     this.length--;
     if (this.length === 0) {
       this.head = null;
@@ -44,119 +41,107 @@ class SinglyLinkedList {
     }
     return current;
   }
+
   shift() {
     if (!this.head) return undefined;
     let temp = this.head;
-    let current = temp.next;
-    this.head = current;
-    this.length--;
+    this.head = this.head.next;
     if (this.length === 0) {
       this.head = null;
       this.tail = null;
     }
-
+    this.length--;
     return temp;
   }
-  unShift(value) {
-    let val = new Node(value); // create the value as linked list
-    if (!this.head) {
-      // no head then tail/head would be same
-      this.head = val;
-      this.tail = val;
-    } else {
-      val.next = this.head;
-      this.head = val; // set head to this value
+  unshift(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = value;
+      this.tail = value;
     }
+
+    let current = this.head;
+    this.head = newNode;
+    newNode.next = current;
     this.length++;
-    return this;
+    return;
   }
   get(index) {
     if (index < 0 || index > this.length) return null;
     let count = 0;
     let current = this.head;
-    while (count !== index) {
+
+    while (index !== count) {
       current = current.next;
       count++;
     }
     return current;
   }
   set(index, value) {
-    // let found = this.get(index);
-    // if (found) {
-    //   found.value = value;
-    //   return true;
-    // }
-    // return false;
-
     if (index < 0 || index > this.length) return false;
-
-    let count = 0;
     let current = this.head;
-
-    while (count < index) {
+    let count = 0;
+    while (count !== index) {
       current = current.next;
       count++;
     }
-    current.value = value; // here we update exact value so that we don't have ponit to anything
-    return true;
+    current.value = value;
   }
   insert(index, value) {
     if (index < 0 || index > this.length) return false;
+    if (index === 0) {
+      return this.unshift(value);
+    }
 
-    if (index === 0) return !!this.unShift(value); // console.log(!'hi')--> false
-    if (this.length === index) return !!this.push(value); // but console.log(!!'hi')--> true
-
+    if (index === this.length - 1) {
+      return this.push(value);
+    }
     let newNode = new Node(value);
     let prev = this.get(index - 1);
     let temp = prev.next;
     prev.next = newNode;
     newNode.next = temp;
     this.length++;
-    return true;
+    return temp;
   }
-
   remove(index) {
     if (index < 0 || index > this.length) return false;
-    if (this.length - 1 === index) return !!this.pop();
-    if (this.length === 0) return !!this.shift();
+    if (index === 0) {
+      return this.shift();
+    }
+    if (index === this.length) {
+      return this.pop();
+    }
 
     let prev = this.get(index - 1);
-    let remove = prev.next;
-    let current = remove.next;
-    prev.next = current;
+    let temp = prev.next;
+    prev.next = temp.next;
     this.length--;
-    return remove;
+    return temp;
   }
+
   reverse() {
     if (!this.head) return undefined;
     let prev = null;
     let current = this.head;
     this.tail = this.head;
-    while (current) {
+    while (current !== null) {
       let next = current.next;
       current.next = prev;
       prev = current;
       current = next;
     }
-    this.head = prev; // now prev would our head
+    this.head = prev;
     return this;
   }
 }
 
-// Example usage
 const list = new SinglyLinkedList();
 list.push(10);
 list.push(20);
 list.push(30);
 // list.pop();
-// list.pop();
-// list.pop();
-// list.shift();
-// console.log(list.get(2));
-// console.log(list.set(1, 15));
-// console.log(list.insert(1, 15));
-// console.log(list.insert(4, 25));
-console.log(list.remove(1));
-// console.log(list.reverse());
-
+// list.unshift(5);
+// console.log(list.remove(1));
+console.log(list.reverse());
 console.log(list);
