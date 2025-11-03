7c0e254dc6aed88b9a545e305f4a395a29d72c84
Partha Das <parthodasm@gmail.com>
2025-11-03 17:25:12 +0600

    Solved 2 Problem

--- 01_Array & String/With Built-in (Default)/01_Group_Anagram.js
+++ 01_Array & String/With Built-in (Default)/01_Group_Anagram.js
@@ -2,16 +2,16 @@ let Group_Anagram = (strs) => {
   let groups = {};
   for (let word of strs) {
     let sorted = word.split("").sort().join("");
-    // console.log(sorted);
+    console.log(sorted);
 
     if (!groups[sorted]) {
       groups[sorted] = [];
     }
     groups[sorted].push(word);
   }
-//   console.log(groups);
+  //   console.log(groups);
 
   return Object.values(groups);
 };
 
-Group_Anagram(["eat", "tea", "tan", "ate", "nat", "bat"]);
+console.log(Group_Anagram(["eat", "tea", "tan", "ate", "nat", "bat"]));
--- 01_Array & String/Without Bulit-in/04_groupAnagram.js
+++ 01_Array & String/Without Bulit-in/04_groupAnagram.js
@@ -1,38 +1,12 @@
-var groupAnagrams = function (strs) {
-  let groups = {}; // we have to create this obj outside of the loop because it's store all words not just only one
-  for (let i = 0; i < strs.length; i++) {
-    // it's run strs length of times
+var groupAnagrams = function(strs) {
+    const groups={}
+    for(let i=0;i<strs.length;i++){
+        let word=strs[i]
+        const sorted=word.split('').sort().join('')
 
-    // in here just for one word only one oparetion happen once at a time
-    let word = strs[i];
-    let count = Array(26).fill(0); // just create one arr once at a time
-
-    for (let k = 0; k < word.length; k++) {
-      let charCode = word.charCodeAt(k);
-      let index = charCode - 97;
-      count[index] += 1; // each time one value of 26 length of arr
+        if(!groups[sorted])groups[sorted]=[]
+        groups[sorted].push(word)
     }
-    let key = ""; // create it as a form of a string
-    for (let k = 0; k < 26; k++) {
-      // run for only one word once at a time
-      key += "#" + count[k];
-    }
-    if (!groups[key]) {
-      groups[key] = []; // 1st create []
-    } // after running if block it's became true, now if we run else block it will not run because if was true so skip the 1st word
-    groups[key].push(word); // now in here key wise push word in that []
-  }
-  // we also have to create result outside of the loop because it's collect the groups key and values
 
-  // let result = [];
-  // let index = 0;
-  // for (let key in groups) {
-  //   result[index] = groups[key];
-  //   index++;
-  // }
-  // return result;
-
-  return Object.values(groups);
+    return Object.values(groups)
 };
-
-console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
