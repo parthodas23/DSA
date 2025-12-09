80fbff89c6be651094f00e7f22da4edc86472e24
Partha Das <parthodasm@gmail.com>
2025-12-09 10:24:31 +0600

    Revised 2 problem

--- 08_Graph/Problems/05_graphValidTree.js
+++ 08_Graph/Problems/05_graphValidTree.js
@@ -1,25 +1,33 @@
 const isValidGraphTree = (n, edges) => {
+  // a graph is a tree if
+  // it's nodes length = n-1
   if (edges.length !== n - 1) return false;
-
-  let graph = Array.from({ length: n }, () => []);
+  // all node's are connected, you can go any node to any other node
+  const graph = {};
   for (let [a, b] of edges) {
+    if (!graph[a]) graph[a] = [];
+    if (!graph[b]) graph[b] = [];
+
     graph[a].push(b);
     graph[b].push(a);
   }
+  // a node can't be appear 2 times
+  let queue = [[-1, 0]];
+  // connected doesn't mean we can't go to the parent, that's why we need skip to go to the parent again
   let visited = new Set();
 
-  let queue = [[-1, 0]];
   while (queue.length) {
     let [parent, node] = queue.shift();
     visited.add(node);
+
     for (let nei of graph[node]) {
       if (nei === parent) continue;
 
       if (visited.has(nei)) return false;
+
       queue.push([node, nei]);
     }
   }
-  console.log(graph);
 
   return true;
 };
