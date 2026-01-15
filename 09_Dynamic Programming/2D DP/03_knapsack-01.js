ffd7d6094eedc4eb321187e72d2c2465e5f3bcb9
Partha Das <parthodasm@gmail.com>
2026-01-15 17:48:34 +0600

    Learn knapsack and lcs, also practiced

---
+++ 09_Dynamic Programming/2D DP/03_knapsack-01.js
@@ -0,0 +1,21 @@
+const knapsack = (weights, values, capacity) => {
+  let n = weights.length;
+  let dp = Array.from({ length: n + 1 }, () => new Array(capacity + 1).fill(0));
+
+  for (let i = 1; i <= n; i++) {
+    for (let w = 1; w <= capacity; w++) {
+      if (weights[i - 1] <= w) {
+        dp[i][w] = Math.max(
+          dp[i - 1][w],
+          values[i - 1] + dp[i - 1][w - weights[i - 1]]
+        );
+      } else {
+        dp[i][w] = dp[i - 1][w];
+      }
+    }
+  }
+
+  return dp[n][capacity];
+};
+
+console.log(knapsack([1, 2, 3], [6, 10, 12], 5));
--- 09_Dynamic Programming/2D DP/04_LCS(longestCommonSequence).js
+++ 09_Dynamic Programming/2D DP/04_LCS(longestCommonSequence).js
@@ -1,14 +1,15 @@
 const lcs = (s1, s2) => {
-  let m = s1.length;
-  let n = s2.length;
+  const m = s1.length;
+  const n = s2.length;
 
   let dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
 
   for (let i = 1; i <= m; i++) {
     for (let j = 1; j <= n; j++) {
       if (s1[i - 1] === s2[j - 1]) {
-        dp[i][j] = 1 + dp[i - 1][j - 1]; // previously we store the count at the same pos
+        dp[i][j] = 1 + dp[i - 1][j - 1];
       } else {
+        // you have to skip char but from which one? s1 or s2
         dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
       }
     }
